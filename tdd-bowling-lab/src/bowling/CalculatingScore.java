846cd4c375ef0052fbee1595fba8cbaa2d19b117
Emil Sandberg <emil.hietanen@gmail.com>
2018-05-17 18:46:47 +0200

    Changed all methods to static

--- tdd-bowling-lab/src/bowling/CalculatingScore.java
+++ tdd-bowling-lab/src/bowling/CalculatingScore.java
@@ -2,7 +2,7 @@ package bowling;
 import java.util.stream.*;
 
 public class CalculatingScore {
-	public int[] createFrame(int throw1, int throw2) throws IllegalArgumentException {
+	public static int[] createFrame(int throw1, int throw2) throws IllegalArgumentException {
 		if (throw1 < 0 || throw1 > 10 || throw2 < 0 || throw2 > 10) {
 			throw new IllegalArgumentException("A throw has to be between 0 and 10");
 		}
@@ -13,7 +13,7 @@ public class CalculatingScore {
 		return frame;
 	}
 
-	public Object[] createGame(int[] frame1, int[] frame2, int[] frame3, int[] frame4, int[] frame5, int[] frame6, int[] frame7, int[] frame8, int[] frame9, int[] frame10) {
+	public static Object[] createGame(int[] frame1, int[] frame2, int[] frame3, int[] frame4, int[] frame5, int[] frame6, int[] frame7, int[] frame8, int[] frame9, int[] frame10) {
 		Object[] game = new Object[10];
 		game[0] = frame1;
 		game[1] = frame2;
--- tdd-bowling-lab/src/bowling/CalculatingScoreTest.java
+++ tdd-bowling-lab/src/bowling/CalculatingScoreTest.java
@@ -11,8 +11,7 @@ class CalculatingScoreTest {
 		int throw1 = 5;
 		int throw2 = 4;
 
-		CalculatingScore scorer = new CalculatingScore();
-		int[] frame = scorer.createFrame(throw1, throw2);
+		int[] frame = CalculatingScore.createFrame(throw1, throw2);
 
 		int[] expectedResult = new int[] {5, 4};
 
@@ -24,9 +23,7 @@ class CalculatingScoreTest {
 		int throw1 = -1;
 		int throw2 = 4;
 
-		CalculatingScore scorer = new CalculatingScore();
-
-		Throwable exception = assertThrows(IllegalArgumentException.class, ()->{scorer.createFrame(throw1, throw2);} );
+		Throwable exception = assertThrows(IllegalArgumentException.class, ()->{CalculatingScore.createFrame(throw1, throw2);} );
 		assertEquals("A throw has to be between 0 and 10", exception.getMessage());
 	}
 
@@ -35,28 +32,24 @@ class CalculatingScoreTest {
 		int throw1 = 5;
 		int throw2 = 8;
 
-		CalculatingScore scorer = new CalculatingScore();
-
-		Throwable exception = assertThrows(IllegalArgumentException.class, ()->{scorer.createFrame(throw1, throw2);} );
+		Throwable exception = assertThrows(IllegalArgumentException.class, ()->{CalculatingScore.createFrame(throw1, throw2);} );
 		assertEquals("The two throws can not be more than 10 combined", exception.getMessage());
 	}
 
 	@Test
 	public void createGame() {
-		CalculatingScore scorer = new CalculatingScore();
+		int[] frame1 = CalculatingScore.createFrame(1, 5);
+		int[] frame2 = CalculatingScore.createFrame(3, 6);
+		int[] frame3 = CalculatingScore.createFrame(7, 2);
+		int[] frame4 = CalculatingScore.createFrame(3, 6);
+		int[] frame5 = CalculatingScore.createFrame(4, 4);
+		int[] frame6 = CalculatingScore.createFrame(5, 3);
+		int[] frame7 = CalculatingScore.createFrame(3, 3);
+		int[] frame8 = CalculatingScore.createFrame(4, 5);
+		int[] frame9 = CalculatingScore.createFrame(8, 1);
+		int[] frame10 = CalculatingScore.createFrame(2, 6);
 
-		int[] frame1 = scorer.createFrame(1, 5);
-		int[] frame2 = scorer.createFrame(3, 6);
-		int[] frame3 = scorer.createFrame(7, 2);
-		int[] frame4 = scorer.createFrame(3, 6);
-		int[] frame5 = scorer.createFrame(4, 4);
-		int[] frame6 = scorer.createFrame(5, 3);
-		int[] frame7 = scorer.createFrame(3, 3);
-		int[] frame8 = scorer.createFrame(4, 5);
-		int[] frame9 = scorer.createFrame(8, 1);
-		int[] frame10 = scorer.createFrame(2, 6);
-
-		Object[] game = scorer.createGame(frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10);
+		Object[] game = CalculatingScore.createGame(frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10);
 
 		Object[] expectedResult = {frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10};
 
@@ -67,8 +60,8 @@ class CalculatingScoreTest {
 	@Test
 	// Should return sum of frame, which is 9 in this case
 	public void testGetFrameScore() {
-		int[] frame    = new CalculatingScore().createFrame(5, 4);
-		int frameScore = new CalculatingScore().getFrameScore(frame);
+		int[] frame    = CalculatingScore.createFrame(5, 4);
+		int frameScore = CalculatingScore.getFrameScore(frame);
 		
 		assertEquals(frameScore, 9);
 	}
