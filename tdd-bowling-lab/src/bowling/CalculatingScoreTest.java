612254365fa7711e0721de511effbeb27d60da73
Emil Sandberg <emil.hietanen@gmail.com>
2018-05-17 21:56:09 +0200

    Test case for user story5

--- tdd-bowling-lab/src/bowling/CalculatingScoreTest.java
+++ tdd-bowling-lab/src/bowling/CalculatingScoreTest.java
@@ -1,5 +1,4 @@
 package bowling;
-import java.util.stream.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
@@ -116,4 +115,23 @@ class CalculatingScoreTest {
 		assertEquals(strikeScore, 17);
 	}
 
+	@Test
+	public void testCalculateGameWithStrike() {
+		int[] frame1 = CalculatingScore.createFrame(10, 0);
+		int[] frame2 = CalculatingScore.createFrame(3, 6);
+		int[] frame3 = CalculatingScore.createFrame(7, 2);
+		int[] frame4 = CalculatingScore.createFrame(3, 6);
+		int[] frame5 = CalculatingScore.createFrame(4, 4);
+		int[] frame6 = CalculatingScore.createFrame(5, 3);
+		int[] frame7 = CalculatingScore.createFrame(3, 3);
+		int[] frame8 = CalculatingScore.createFrame(4, 5);
+		int[] frame9 = CalculatingScore.createFrame(8, 1);
+		int[] frame10 = CalculatingScore.createFrame(2, 6);
+
+		int[][] game = CalculatingScore.createGame(frame1, frame2, frame3, frame4, frame5, frame6, frame7, frame8, frame9, frame10);
+
+		int gameScore = CalculatingScore.calculateGameScoreWithStrikes(game);
+
+		assertEquals(gameScore, 94);
+	}
 }
