bc718961b1a3cbf1232af6ecd5a1cb46fd66a03f
Emil Sandberg <emil.hietanen@gmail.com>
2018-05-17 16:46:59 +0200

    Refactoring of user story 1: reduced unnecessary lines of code and specified exception types

--- tdd-bowling-lab/src/bowling/CalculatingScore.java
+++ tdd-bowling-lab/src/bowling/CalculatingScore.java
@@ -1,16 +1,14 @@
 package bowling;
 
 public class CalculatingScore {
-	public int[] createFrame(int throw1, int throw2) throws Exception {
+	public int[] createFrame(int throw1, int throw2) throws IllegalArgumentException {
 		if (throw1 < 0 || throw1 > 10 || throw2 < 0 || throw2 > 10) {
-			throw new Exception("A throw has to be between 0 and 10");
+			throw new IllegalArgumentException("A throw has to be between 0 and 10");
 		}
 		if ((throw1 + throw2) > 10) {
-			throw new Exception("The two throws can not be more than 10 combined");
+			throw new IllegalArgumentException("The two throws can not be more than 10 combined");
 		}
-		int[] frame = new int[2];
-		frame[0] = throw1;
-		frame[1] = throw2;
+		int[] frame = {throw1, throw2};
 		return frame;
 	}
 }
