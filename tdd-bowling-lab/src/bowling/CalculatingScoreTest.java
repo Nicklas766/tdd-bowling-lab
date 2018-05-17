002e22392c9c6521e83ed795e367c09205c57c37
Nicklas766 <Nicklas766@live.se>
2018-05-17 13:45:21 +0200

    Setup

---
+++ tdd-bowling-lab/src/bowling/CalculatingScoreTest.java
@@ -0,0 +1,14 @@
+package bowling;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatingScoreTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
